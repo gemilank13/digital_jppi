973170da976a9412d388e5e0ccb68630aee76aa7
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2023-07-18 10:52:12 +0700

    Delete ab_komersial directory

--- ab_komersial/models/inherit_product.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-class InheritProduct(models.Model):
-    _inherit = 'product.template'
-
-    custprice_line_ids = fields.One2many('cust.price', 'product_id', string="Customer Price")
