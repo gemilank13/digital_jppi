25c94b2bc0288ba2c887ec65fa51f1d01d346a0d
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2023-08-04 11:03:24 +0700

    Add files via upload

---
+++ ab_komersial_report/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Komersial Report',
+    'version': '12.0.1.0.0',
+    'summary': 'Report Digitalisasi JPPI Komersial',
+    'description': '',
+    'category': 'Extra Tools',
+    'author': 'Geger Gemilank - PT JPPI',
+    'website': '-',
+    # 'license': 'AGPL',
+    'depends': ['ab_komersial'],
+    'data': [
+        'views/report.xml',
+        'views/report_komersial.xml',
+        
+    ],
+    'demo': [''],
+    'installable': True,
+    'auto_install': False,
+}
