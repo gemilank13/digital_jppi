d5cc5628866cfa741fce853e2df8164307d87202
gemilank13 <39510051+gemilank13@users.noreply.github.com>
2023-08-16 13:25:34 +0700

    Delete ab_komersial directory

--- ab_komersial/models/inherit_users.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-class InheritUsers(models.Model):
-    _inherit = 'res.users'
-
-    lokasi_id = fields.Many2one('res.partner', string="Lokasi Cabang")
